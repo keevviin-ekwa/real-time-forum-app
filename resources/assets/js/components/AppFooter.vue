715aac8ede975478ccace9a0dc57055ac87e6df7
KEVIN EKWA <ekwabayard@gmail.com>
2021-03-29 09:17:48 +0100

    vuetify vue, vue router

---
+++ resources/assets/js/components/AppFooter.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-footer
+        dark
+        height="auto"
+    >
+        <v-card
+            class="flex"
+            flat
+            tile
+        >
+
+            <v-card-actions class="grey darken-3 justify-center">
+                &copy;{{new Date().getFullYear()}}— <strong>By Kevin Ekwa</strong>
+            </v-card-actions>
+        </v-card>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    name: "AppFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
